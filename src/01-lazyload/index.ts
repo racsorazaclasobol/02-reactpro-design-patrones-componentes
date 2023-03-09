b6aa437217e86f625d0437d44966e4b2a9d4aa2f
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-03-09 13:08:14 -0300

    lazyload por componente terminado

--- src/01-lazyload/index.ts
+++
@@ -1,3 +0,0 @@
-export { LazyPage1 } from './pages/LazyPage1'
-export { LazyPage2 } from './pages/LazyPage2'
-export { LazyPage3 } from './pages/LazyPage3'
---
+++ src/01-lazyload/pages/NoLazy.tsx
@@ -0,0 +1,9 @@
+
+
+export const NoLazy = () => {
+    return (
+        <div>No Lazy Component</div>
+    )
+}
+
+export default NoLazy;
