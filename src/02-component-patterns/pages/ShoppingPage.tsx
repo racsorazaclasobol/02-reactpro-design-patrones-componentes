c610543848ca5c0b1da8290b0c1e397a1d992e28
Oscar Alcazar <oscar.alcazar.lobos@gmail.com>
2023-03-13 19:59:57 -0300

    Se limpia para implementar el patron State Initializer

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,55 +1,25 @@
 import { ProductButton, ProductImage, ProductCard, ProductTitle } from '../components/';
 
 import '../styles/custom-styles.css';
-import { useShoppingCart } from '../hooks/useShoppingCart';
-import { useProduct } from '../hooks/useProduct';
 import { products } from '../data/products';
+
+const product = products[0];
 
 export const ShoppingPage = () => {
 
-	const { shoppingCart, onProductCountChange } = useShoppingCart(); 
 
     return (
 		<div>
 			<h1>Shpping Store</h1>
 			<hr />
 
-			<div style={{ display: 'flex', flexDirection:'row', flexWrap: 'wrap' }}>
+			<ProductCard key={ product.id } product={ product } className="bg-dark text-white" >
 
-				{
-					products.map( product => (
-						<ProductCard key={ product.id } product={ product } counterValue={ shoppingCart[product.id]?.counter || 0 } onChange={ onProductCountChange } className="bg-dark text-white" >
+				<ProductImage className="custom-image" />
+				<ProductTitle className="text-bold" />
+				<ProductButton className="custom-buttons" />
 
-							<ProductImage className="custom-image" />
-							<ProductTitle className="text-bold" />
-							<ProductButton className="custom-buttons" />
-
-						</ProductCard>
-					))
-				}
-
-			</div>
-			
-			<div className='shopping-cart'>
-
-				{
-					Object.entries( shoppingCart ).map( ([ key, product ]) => (
-						<ProductCard key={ key } product={ product } counterValue={ product.counter } onChange={ onProductCountChange } className="bg-dark text-white" style={{ width: '100px' }} >
-
-							<ProductImage className="custom-image" />
-							<ProductButton className="custom-buttons" />
-
-						</ProductCard>
-					))
-				}
-
-			</div>
-
-			<div>
-				<code>
-					{ JSON.stringify( shoppingCart, null, 4 ) }
-				</code>
-			</div>
+			</ProductCard>
 
 		</div>
     )
